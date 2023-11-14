6e91cc745e628d531ad6c9bc019dcae86c791f48
Kakha <kakhachankseliani@gmail.com>
2023-11-14 13:06:02 +0400

    feat: created a simple connection to mysql db

--- index.php
+++ index.php
@@ -2,3 +2,14 @@
 
 require 'functions.php';
 require 'router.php';
+
+$dsn = 'mysql:host=localhost;port=3306;dbname=scandishop;user=chanksela;password=popgof;charset=utf8mb4';
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM products");
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+// dd($products);
+foreach ($products as $product) {
+    echo "<li>" . $product['name'] - $product['price'] . "</li>";
+}
