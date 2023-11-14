04b7d21b46c630f290df9b9e4738617d595627e5
Kakha <kakhachankseliani@gmail.com>
2023-11-14 14:14:46 +0400

    feat: displaying products from db

--- index.php
+++ index.php
@@ -2,14 +2,4 @@
 
 require 'functions.php';
 require 'Database.php';
-// require 'router.php';
-
-$config = require 'config.php';
-
-$db = new Database($config['database']);
-
-$query = "SELECT * FROM products";
-$products = $db->query($query)->fetchAll();
-foreach ($products as $product) {
-    echo "<li>" . $product['name'] . " - " . $product['price'] . "</li>";
-}
+require 'router.php';
