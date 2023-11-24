af12e405b59b9b14fd6424874133c6ceceb18c88
Kakha <kakhachankseliani@gmail.com>
2023-11-24 16:56:26 +0400

    style: added very simple styles

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,4 +1,9 @@
-<nav>
-  <li><a href="/">Home</a></li>
-  <li><a href="/add-item">Add Item</a></li>
+<nav class="navigation">
+  <div class="page-name">
+    <h1>Product List</h1>
+  </div>
+  <ul class="menu">
+    <li><a href="/add-item">Add</a></li>
+    <li><a href="/">Mass Delete</a></li>
+  </ul>
 </nav>
