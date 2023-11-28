2e126d89933932dcbd63b1b58af9b2fb17802d71
Kakha <kakhachankseliani@gmail.com>
2023-11-28 16:22:42 +0400

    wip: added simple CORS settings to be able to use routes on vue frontend

--- public/index.php
+++ public/index.php
@@ -7,6 +7,13 @@ spl_autoload_register(function ($class) {
     $class = str_replace('\\', '/', $class);
     require base_path("{$class}.php");
 });
+
+
+header("Access-Control-Allow-Origin: http://localhost:5173"); // Replace with your Vue app's URL
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
 $router = new \Core\Router();
 
 $routes = require base_path('routes.php');
