503f8dd7174d57f452cac0d02be4eac30c074192
Kakha <kakhachankseliani@gmail.com>
2023-11-30 15:01:40 +0400

    feat/wip: * set up backend to receive data from frontend * added new headers to receive CONTENT TYPE for CORS * for now echoing received datas

--- controllers/destroy.php
+++ controllers/destroy.php
@@ -9,7 +9,16 @@
 // $query = "SELECT * FROM products";
 // $products = $db->query($query)->fetchAll();
 
-// echo json_encode(['message' => 'Products deleted']);
+// Getting raw request body
+$rawData = file_get_contents("php://input");
+// JSON data to associate array
+$data = json_decode($rawData, true);
 
-
-echo $_SERVER['REQUEST_METHOD'];
+// Check if data exists
+if ($data) {
+    //  echo data of ids to be deleted
+    echo json_encode(['message' => 'Received IDs for deletion: ' . implode(', ', $data['ids'])]);
+} else {
+    // in case something went wrong with data echo error message
+    echo json_encode(['error' => 'Invalid request or missing data']);
+}
