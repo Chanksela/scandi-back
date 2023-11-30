f6fe9123acdad05bf162f6c082b4e11755e80eb3
Kakha <kakhachankseliani@gmail.com>
2023-11-30 15:45:55 +0400

    feat: * added conditional logic to check if delete is possible * can delete one or several products by ids * added comments to make logic more readable

--- controllers/destroy.php
+++ controllers/destroy.php
@@ -1,13 +1,12 @@
 <?php
 
-// use Core\Database;
+use Core\Database;
 
-// $config = require base_path('config.php');
+// require configs for db connection
+$config = require base_path('config.php');
 
-
-// $db = new Database($config['database']);
-// $query = "SELECT * FROM products";
-// $products = $db->query($query)->fetchAll();
+// establish connection to db
+$db = new Database($config['database']);
 
 // Getting raw request body
 $rawData = file_get_contents("php://input");
@@ -16,9 +15,32 @@ $data = json_decode($rawData, true);
 
 // Check if data exists
 if ($data) {
-    //  echo data of ids to be deleted
-    echo json_encode(['message' => 'Received IDs for deletion: ' . implode(', ', $data['ids'])]);
+    // create query to select existing IDs from the database
+    $query = "SELECT id FROM products WHERE id IN (" . implode(',', $data['ids']) . ")";
+    // execute the query
+    $result = $db->query($query);
+    // fetch the IDs from the result
+    $existingIds = $result->fetchAll(PDO::FETCH_COLUMN);
+
+    // check if all received IDs exist in the database
+    if (count($existingIds) > 0) {
+        // create query to delete data by IDs
+        $query = "DELETE FROM products WHERE id IN (" . implode(',', $data['ids']) . ")";
+        // delete data from the database depending on received IDs
+        $db->query($query);
+        // echo success message
+        echo json_encode(['success' => 'Products deleted successfully']);
+        // Set the response status code to 200 (OK)
+        http_response_code(200);
+    } else {
+        // in case some IDs do not exist in the database, echo error message
+        echo json_encode(['error' => 'Invalid request or missing data']);
+        // Set the response status code to 400 (Bad Request)
+        http_response_code(400);
+    }
 } else {
-    // in case something went wrong with data echo error message
-    echo json_encode(['error' => 'Invalid request or missing data']);
+    // in case no data received, echo error message
+    echo json_encode(['error' => 'Something wen wrong']);
+    // Set the response status code to 400 (Bad Request)
+    http_response_code(400);
 }
