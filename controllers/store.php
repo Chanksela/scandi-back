02ed088922ff8bbd8af23fa6455b1feee5c910dc
Kakha <kakhachankseliani@gmail.com>
2023-12-05 10:44:38 +0400

    refactor: small refactor for store controller

--- controllers/store.php
+++ controllers/store.php
@@ -10,37 +10,26 @@ $db = new Database($config['database']);
 $rawData = file_get_contents("php://input");
 // JSON data to associate array
 $data = json_decode($rawData, true);
-// dd($data);
+
+$params = [
+      'sku' => $data['sku'],
+      'name' => $data['name'],
+      'price' => $data['price'],
+      'productType' => $data['productType'],
+  ];
+
 if($data['productType'] === "1") {
     $query = "INSERT INTO products (sku, name, price, type_id, parameters) VALUES (:sku, :name, :price, :productType, :parameters)";
-    $params = [
-        'sku' => $data['sku'],
-        'name' => $data['name'],
-        'price' => $data['price'],
-        'productType' => $data['productType'],
-        'parameters' => $data['size']
-    ];
+    $params['parameters'] = [$data['size']];
     $db->query($query, $params);
 } elseif($data['productType'] === "2") {
     $query = "INSERT INTO products (sku, name, price, type_id, parameters) VALUES (:sku, :name, :price, :productType, :parameters)";
-    $params = [
-        'sku' => $data['sku'],
-        'name' => $data['name'],
-        'price' => $data['price'],
-        'productType' => $data['productType'],
-        'parameters' => $data['weight']
-    ];
+    $params['parameters'] = $data['weight'];
     $db->query($query, $params);
 
 } elseif ($data['productType'] === "3") {
     $query = "INSERT INTO products (sku, name, price, type_id, parameters) VALUES (:sku, :name, :price, :productType, :parameters)";
-    $params = [
-        'sku' => $data['sku'],
-        'name' => $data['name'],
-        'price' => $data['price'],
-        'productType' => $data['productType'],
-        'parameters' => $data['height'] . "x" . $data['width'] . "x" . $data['length']
-    ];
+    $params['parameters'] = [$data['height'], $data['width'], $data['length']];
     $db->query($query, $params);
 } else {
     echo json_encode(['error' => 'Something wen wrong']);
