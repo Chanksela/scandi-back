2bfa9fb0156c12e786663da791b70dd7e3a0ef60
Kakha <kakhachankseliani@gmail.com>
2023-12-07 18:04:43 +0400

    fix

--- index.php
+++ index.php
@@ -11,9 +11,8 @@ header("Access-Control-Allow-Origin: https://scand-client.vercel.app/");
 header("Access-Control-Allow-Methods: GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    dd('test');
-    header("Access-Control-Allow-Origin: https://scand-client.vercel.app/");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: https://scand-client.vercel.app");
     header("Access-Control-Allow-Methods: GET, POST, DELETE");
     header("Access-Control-Allow-Headers: Content-Type");
     http_response_code(200);
