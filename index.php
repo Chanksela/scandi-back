6d88fa78987a34b886daf0f55edb443e4716c69e
Kakha <kakhachankseliani@gmail.com>
2023-12-07 17:12:51 +0400

    test

--- index.php
+++ index.php
@@ -1,28 +1,29 @@
 <?php
 
-const BASE_PATH = __DIR__ . '/';
-require BASE_PATH . 'Core/functions.php';
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', '/', $class);
-    require base_path("{$class}.php");
-});
+echo "Hello World";
+// const BASE_PATH = __DIR__ . '/';
+// require BASE_PATH . 'Core/functions.php';
+// spl_autoload_register(function ($class) {
+//     $class = str_replace('\\', '/', $class);
+//     require base_path("{$class}.php");
+// });
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type");
-    http_response_code(200);
-    exit;
-}
-header("Access-Control-Allow-Origin: *");
+// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+//     header("Access-Control-Allow-Origin: *");
+//     header("Access-Control-Allow-Methods: GET, POST, DELETE");
+//     header("Access-Control-Allow-Headers: Content-Type");
+//     http_response_code(200);
+//     exit;
+// }
+// header("Access-Control-Allow-Origin: *");
 
 
 
 
-$router = new \Core\Router();
+// $router = new \Core\Router();
 
-$routes = require base_path('routes.php');
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-$method = $_SERVER['REQUEST_METHOD'];
+// $routes = require base_path('routes.php');
+// $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+// $method = $_SERVER['REQUEST_METHOD'];
 
-$router->route($uri, $method);
+// $router->route($uri, $method);
