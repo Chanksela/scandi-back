dc058cb415406db50c84fc684a0c695a51f1c794
Kakha <kakhachankseliani@gmail.com>
2023-12-07 17:52:54 +0400

    adasd

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ header("Access-Control-Allow-Methods: GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: http://localhost:5173/");
     header("Access-Control-Allow-Methods: GET, POST, DELETE");
     header("Access-Control-Allow-Headers: Content-Type");
     http_response_code(200);
