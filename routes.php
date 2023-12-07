61c67ea4dcce13bf11194f06f5928fbf04e43fa7
Kakha <kakhachankseliani@gmail.com>
2023-12-07 17:10:38 +0400

    moved index.php

--- routes.php
+++ routes.php
@@ -1,10 +1,6 @@
 <?php
 
 
-// $routes = [
-//     '/' => 'controllers/index.php',
-//     '/add-item' => 'controllers/create.php'
-// ];
 
 $router->get('/', 'controllers/index.php');
 $router->delete('/product', 'controllers/destroy.php');
