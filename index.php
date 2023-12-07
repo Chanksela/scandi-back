65441fb06f9bf43f2271e11e0094e79bd0d286f5
Kakha <kakhachankseliani@gmail.com>
2023-12-07 17:36:18 +0400

    headers fix

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ spl_autoload_register(function ($class) {
 });
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: https://scand-client.vercel.app/");
     header("Access-Control-Allow-Methods: GET, POST, DELETE");
     header("Access-Control-Allow-Headers: Content-Type");
     http_response_code(200);
