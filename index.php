54b52c94a47714f58c5318ddacb3ef8746693d71
Kakha <kakhachankseliani@gmail.com>
2023-12-08 15:44:02 +0400

    headers in index

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-// header("Access-Control-Allow-Origin: https://scand-client.vercel.app");
-// header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
-// header("Access-Control-Allow-Headers: Content-Type");
-// header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
 
 const BASE_PATH = __DIR__ . '/';
 require BASE_PATH . 'Core/functions.php';
