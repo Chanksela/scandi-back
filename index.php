e8691f8a0ef23f0c0f6db37edfe1e1c4bf053f65
Kakha <kakhachankseliani@gmail.com>
2023-12-08 11:46:26 +0400

    wip: small change in CORS headers url

--- index.php
+++ index.php
@@ -8,14 +8,14 @@ spl_autoload_register(function ($class) {
 });
 
 
-header("Access-Control-Allow-Origin: https://scand-client.vercel.app/");
+header("Access-Control-Allow-Origin: https://scand-client.vercel.app");
 header("Access-Control-Allow-Methods: GET, OPTIONS, POST, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Origin: https://scand-client.vercel.app/");
+    header("Access-Control-Allow-Origin: https://scand-client.vercel.app");
     header("Access-Control-Allow-Methods: GET, OPTIONS, POST, DELETE");
     header("Access-Control-Allow-Headers: Content-Type");
     header("Access-Control-Allow-Credentials: true");
