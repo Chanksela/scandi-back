9a41557a1b7f162e5a9a32bef0c2a118d699cbfb
Kakha <kakhachankseliani@gmail.com>
2023-12-08 14:03:52 +0400

    wip: testing if new routes work

--- routes.php
+++ routes.php
@@ -3,5 +3,6 @@
 
 
 $router->get('/', 'controllers/index.php');
+$router->get('/product', 'controllers/show.php');
 $router->delete('/product', 'controllers/destroy.php');
 $router->post('/product', 'controllers/store.php');
