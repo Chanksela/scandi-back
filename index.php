c8720e121ae97702e934f5b5666565868a161dfa
Kakha <kakhachankseliani@gmail.com>
2023-12-08 15:58:54 +0400

    changed content-length

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
-header("Content-Length: 1000");
+header("Content-Length: 2");
 
 const BASE_PATH = __DIR__ . '/';
 require BASE_PATH . 'Core/functions.php';
