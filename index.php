4854538b3afa42a46773eec0cadf44da1dc4079e
Kakha <kakhachankseliani@gmail.com>
2023-12-08 11:21:52 +0400

    wip: trying to fix CORS error with .htaccess

--- index.php
+++ index.php
@@ -10,9 +10,10 @@ spl_autoload_register(function ($class) {
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Origin: https://scand-client.vercel.app");
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, DELETE");
     header("Access-Control-Allow-Headers: Content-Type");
+    header('Cache-Control: max-age=604800');
     http_response_code(200);
     exit;
 }
