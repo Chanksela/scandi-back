b98267735446c177ae43a926b725a03a60d85f37
Kakha <kakhachankseliani@gmail.com>
2023-12-08 11:50:36 +0400

    wip: rmeoved extra headers

--- index.php
+++ index.php
@@ -14,15 +14,6 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Origin: https://scand-client.vercel.app");
-    header("Access-Control-Allow-Methods: GET, OPTIONS, POST, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type");
-    header("Access-Control-Allow-Credentials: true");
-    header('Cache-Control: max-age=604800');
-    http_response_code(200);
-    exit;
-}
 
 
 $router = new \Core\Router();
