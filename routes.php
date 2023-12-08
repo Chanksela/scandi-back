5abba2418f2cf6597466f736acec9fd767fd6471
Kakha <kakhachankseliani@gmail.com>
2023-12-08 14:06:55 +0400

    wip: test

--- routes.php
+++ routes.php
@@ -3,6 +3,6 @@
 
 
 $router->get('/', 'controllers/index.php');
-$router->get('/product', 'controllers/show.php');
+$router->get('/test', 'controllers/show.php');
 $router->delete('/product', 'controllers/destroy.php');
 $router->post('/product', 'controllers/store.php');
