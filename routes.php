c56026c3fe612fff7c4bc5627e6728a92384976e
Kakha <kakhachankseliani@gmail.com>
2023-12-08 16:11:42 +0400

    changed route

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 
 
 
-$router->get('/', 'controllers/index.php');
+$router->get('/product', 'controllers/index.php');
 $router->get('/test', 'controllers/show.php');
 $router->delete('/product', 'controllers/destroy.php');
 $router->post('/product', 'controllers/store.php');
