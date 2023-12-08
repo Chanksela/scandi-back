775a775387b674e305dbf171769c0313d89d2a22
Kakha <kakhachankseliani@gmail.com>
2023-12-08 11:36:06 +0400

    wip: added creditentials

--- index.php
+++ index.php
@@ -13,14 +13,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header("Access-Control-Allow-Origin: https://scand-client.vercel.app/");
     header("Access-Control-Allow-Methods: GET, OPTIONS, POST, DELETE");
     header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Credentials: true");
     header('Cache-Control: max-age=604800');
     http_response_code(200);
     exit;
 }
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, DELETE");
+header("Access-Control-Allow-Methods: GET, OPTIONS, POST, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
 
 $router = new \Core\Router();
 
