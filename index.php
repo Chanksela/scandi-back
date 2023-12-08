49b32ce31069d78d5d8d65eda7d09fb8c00a644b
Kakha <kakhachankseliani@gmail.com>
2023-12-08 11:30:09 +0400

    wip: addec specific URL for OPTIONS

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ spl_autoload_register(function ($class) {
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: https://scand-client.vercel.app/");
     header("Access-Control-Allow-Methods: GET, OPTIONS, POST, DELETE");
     header("Access-Control-Allow-Headers: Content-Type");
     header('Cache-Control: max-age=604800');
