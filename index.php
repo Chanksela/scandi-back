87ab05d180dc775c66bfa397a300c4063c3a662a
Kakha <kakhachankseliani@gmail.com>
2023-12-08 16:15:26 +0400

    chore

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://scand-client.vercel.app");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
+
+if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 const BASE_PATH = __DIR__ . '/';
 require BASE_PATH . 'Core/functions.php';
